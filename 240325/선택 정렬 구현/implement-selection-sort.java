19d6ad21522e171572502541a9278ba7e36104f6
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-03-25 01:15:10 +0900

    Title: 선택 정렬 구현, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240325/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i<n; i++){
+            int t = sc.nextInt();
+            arr[i] = t;
+        }
+
+        sort(arr);
+
+        for (int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void sort(int[] array){
+
+        
+        for (int i = 0; i<array.length; i++){
+
+            int min = array[i]; 
+            int index = i;
+
+            for (int j = i+1; j<array.length; j++){
+                if(min > array[j]){
+                    min = array[j];
+                    index = j;
+                } 
+            }
+
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+            
+
+        }
+        
+    }
+}
