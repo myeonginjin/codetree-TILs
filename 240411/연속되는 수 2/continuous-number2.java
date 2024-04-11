960091c57e48b0ced54423be59d00722ed266643
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-04-11 23:34:02 +0900

    Title: 연속되는 수 2, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240411/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int maxCnt = 0;
+        int cnt = 1;
+
+        int n = sc.nextInt();
+        
+
+        int fir = sc.nextInt();
+        int point = fir;
+
+        for (int i = 1; i<n; i++){
+            int num = sc.nextInt();
+            if (fir == num){
+                cnt++;
+
+                maxCnt = Math.max(maxCnt, cnt);
+            }
+            else{
+                fir = num;
+                cnt = 1;
+            }
+        }
+
+        System.out.print(maxCnt);
+    }
+}
