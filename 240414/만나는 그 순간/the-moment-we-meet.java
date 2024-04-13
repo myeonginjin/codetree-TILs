c30a01bebd631cb4ce27b598269de574a7b0c317
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-04-14 01:15:42 +0900

    Title: 만나는 그 순간, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240414/만나는 그 순간/the-moment-we-meet.java
+++ 240414/만나는 그 순간/the-moment-we-meet.java
@@ -52,7 +52,7 @@ public class Main  {
         
         int answer = -1;
 
-        for (int i = 1; i<=time; i++){
+        for (int i = 1; i<time; i++){
             if(aSpot[i] == bSpot[i]) {
                 
                 answer = i;
@@ -60,16 +60,16 @@ public class Main  {
             }
         }
 
-        for (int i = 0; i<time; i++){
-            System.out.print(aSpot[i]+" ");
+        // for (int i = 0; i<=time; i++){
+        //     System.out.print(aSpot[i]+" ");
             
-        }
-        System.out.println();
-        for (int i = 0; i<=time; i++){
-            System.out.print(bSpot[i]+" ");
+        // }
+        // System.out.println();
+        // for (int i = 0; i<=time; i++){
+        //     System.out.print(bSpot[i]+" ");
             
-        }
-        System.out.println();
+        // }
+        // System.out.println();
 
         System.out.print(answer);
     }
