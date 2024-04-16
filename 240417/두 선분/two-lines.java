6be2491db9de5576f9d2b0b6ab44ddca7ccb193f
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-04-17 00:53:34 +0900

    Title: 두 선분, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240417/두 선분/two-lines.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int x4 = sc.nextInt();
+        String str;
+
+        str = x3 <= x1 && x1 <= x4 ? "intersecting" : "nonintersecting";
+        str = x1 <= x3 && x3 <= x2 ? "intersecting" : "nonintersecting";
+
+        System.out.print(str);
+    }
+}
