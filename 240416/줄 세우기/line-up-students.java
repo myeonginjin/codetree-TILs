d73c925c8a221234017de75eddbecbe37a6f0322
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-04-16 23:40:41 +0900

    Title: 줄 세우기, Time: 134ms, Memory: 9MB, Status: Passed - CodeTree

--- 240416/줄 세우기/line-up-students.java
+++ 240416/줄 세우기/line-up-students.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.StringTokenizer;
 import java.lang.StringBuilder;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
 
 class Stu implements Comparable<Stu> {
     int n;
@@ -35,6 +37,7 @@ public class Main {
         StringTokenizer st;
         String str;
         StringBuilder sb = new StringBuilder();
+        ArrayList<Stu> arr2 = new ArrayList<>();
 
         int n = Integer.parseInt(br.readLine());
         Stu[] arr = new Stu[n];
@@ -47,12 +50,22 @@ public class Main {
             int kg = Integer.parseInt(st.nextToken());
 
             arr[i-1] = new Stu(i, cm, kg);
+
+            Stu s = new Stu(i, cm, kg);
+            arr2.add(s);
         }
 
         Arrays.sort(arr);
 
-        for (int i = 0; i<n; i++){
-            sb.append(arr[i].c+" "+arr[i].k+" "+arr[i].n);
+        // for (int i = 0; i<n; i++){
+        //     sb.append(arr[i].c+" "+arr[i].k+" "+arr[i].n);
+        //     sb.append("\n");
+        // }
+
+        Collections.sort(arr2);
+
+        for (int i = 0; i<arr2.size(); i++){
+            sb.append(arr2.get(i).c+" "+arr2.get(i).k+" "+arr2.get(i).n);
             sb.append("\n");
         }
 
