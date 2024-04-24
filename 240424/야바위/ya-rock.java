a206eab068f26ea5624c43e50a89384d6ce15c37
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-04-24 23:26:53 +0900

    Title: 야바위, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240424/야바위/ya-rock.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] process = new int[300];
+    public static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        int index = 0;
+        for (int i = 0; i<n; i++){
+            process[index++] = sc.nextInt();
+            process[index++] = sc.nextInt();
+            process[index++] = sc.nextInt();
+        }
+
+        int ans = getAnswer();
+
+        System.out.print(ans);
+    }
+    
+    public static int getAnswer( ) {
+        int maxCnt = 0;
+
+        for (int i = 0; i<3; i++){
+            int[] cup = new int[4];
+            int cnt = 0;
+
+            cup[i] = 1;
+
+            for (int j = 0; j<n*3-2; j+=3){
+                int temp = cup[process[j]];
+                cup[process[j]] = cup[process[j+1]];
+                cup[process[j+1]] = temp;
+
+                if (cup[process[j+2]] != 1) {
+                    cnt++;
+                }
+                else{ 
+                    maxCnt = Math.max(maxCnt, cnt);
+                    break;
+                }
+            }
+        }
+
+        return maxCnt;
+    }
+}
