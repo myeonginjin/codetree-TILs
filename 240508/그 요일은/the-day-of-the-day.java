05dfd1270f4792e676d226efaffa88d540a29354
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-08 23:48:14 +0900

    Title: 그 요일은, Time: 157ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240508/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        String week = sc.next();
+
+        int[] days = new int[]{31,29,31,30,31,30,31,31,30,31,30,31};
+
+        int orijinDate =0;
+        int newDate =0;
+
+        for (int i = 0; i<m1-1; i++){
+            orijinDate += days[i];
+        }
+        orijinDate += d1;
+
+        for (int i = 0; i<m2-1; i++){
+            newDate += days[i];
+        }
+        newDate += d2;
+
+        int dif = newDate - orijinDate;
+
+        int cnt = 0;
+
+        cnt += dif / 7;
+
+
+        int rest = dif % 7;
+
+        int weekIndex = -1;
+        String[] weeks = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        for (int i = 0; i<7; i++){
+            if(week == weeks[i]){
+                weekIndex = i;
+            }
+        }
+
+        if (weekIndex <= rest){
+            cnt++;
+        }
+
+        System.out.println(cnt);
+
+    }
+}
+
+/*
+5 6 7 8 9 10 11
+12 
+*/
