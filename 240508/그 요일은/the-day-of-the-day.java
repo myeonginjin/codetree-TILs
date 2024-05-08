219c8a1147189cfa12845efe192f1b1c204624e7
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-08 23:54:16 +0900

    Title: 그 요일은, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240508/그 요일은/the-day-of-the-day.java
+++ 240508/그 요일은/the-day-of-the-day.java
@@ -39,7 +39,7 @@ public class Main {
         String[] weeks = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
         for (int i = 0; i<7; i++){
-            if(week == weeks[i]){
+            if(week.equals(weeks[i])){
                 weekIndex = i;
             }
         }
