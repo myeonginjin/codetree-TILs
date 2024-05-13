375bcdd8132425bfb90389005749244d3abdfd1b
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-14 00:00:46 +0900

    Title: 왔다 갔던 구역 2, Time: 113ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240514/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[2001];
+
+        int curX = 1000;
+
+        for (int i = 0; i<n; i++){
+            int dis = sc.nextInt();
+            char dir = sc.next().charAt(0);
+
+            if (dir == 'R'){
+                for (int j = curX+1; j<=curX+dis; j++){
+                    arr[j]+=1;
+                    
+                }
+                curX += dis;
+            }
+            else {
+                for (int j = curX-1; j>=curX-dis; j--){
+                    arr[j]+=1;
+                    
+                }
+                curX -= dis;
+            }
+        }
+        int cnt = 0; 
+        for (int i = 0; i<arr.length; i++){
+            if(arr[i]>=2){
+                cnt++;
+            }
+            // System.out.print(arr[i] +" ");
+        }
+
+        System.out.print(cnt);
+
+
+    }
+}
