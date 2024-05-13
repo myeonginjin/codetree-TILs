75d1cb0979dfbb1c5763c30b782fd97c7ee69628
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-13 23:59:09 +0900

    Title: 왔다 갔던 구역 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240513/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240513/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -28,7 +28,7 @@ public class Main {
             }
         }
         int cnt = 0; 
-        for (int i = 988; i<1004; i++){
+        for (int i = 0; i<arr.length; i++){
             if(arr[i]>=2){
                 cnt++;
             }
