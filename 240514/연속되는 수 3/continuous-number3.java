4fd5220afea3d85a0f37429a973d8fb5d2de6712
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-14 15:26:18 +0900

    Title: 연속되는 수 3, Time: 108ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240514/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int firN = Integer.parseInt(br.readLine());
+        boolean minus = firN > 0 ? false : true;
+        int maxLen = 0;
+        int len = 1;
+
+        for (int i = 1; i<n; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(minus && num < 0){
+                len++;
+            }
+            else if (!minus && num > 0){
+                len++;
+            }
+
+            else {
+                maxLen = Math.max(len, maxLen);
+                len = 1;
+                minus = num > 0 ? false : true;
+            }
+        }
+
+        System.out.print(maxLen);
+
+        
+    }
+}
