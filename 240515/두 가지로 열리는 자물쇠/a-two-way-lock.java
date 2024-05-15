09e211747ed16750929b833a53f6d598a5cf0493
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-15 16:58:37 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240515/두 가지로 열리는 자물쇠/a-two-way-lock.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+        ArrayList[] firArr = new ArrayList[3];
+        ArrayList[] secArr = new ArrayList[3];
+
+        for (int i = 0; i<3; i++){
+            int num = sc.nextInt();
+            ArrayList<Integer> t = new ArrayList<>();
+            for (int j = num; j<=num+2; j++){
+                int temp = j % range;
+                t.add(temp);
+            }
+            for (int j = num-1; j>=num-2; j--){
+                int temp = ((range + j) % range);
+                if(temp==0) temp = range;
+                t.add(temp);
+            }
+            firArr[i] = t;
+        }
+
+        for (int i = 0; i<3; i++){
+            int num = sc.nextInt();
+            ArrayList<Integer> t = new ArrayList<>();
+            for (int j = num; j<=num+2; j++){
+                int temp = j % range;
+                t.add(temp);
+            }
+            for (int j = num-1; j>=num-2; j--){
+                int temp = ((range + j) % range);
+                if(temp==0) temp = range;
+                t.add(temp);
+            }
+            secArr[i] = t;
+        }
+
+        // for (int i = 0; i<3; i++){
+        //     ArrayList<Integer> a = firArr[i];
+        //     for (int j = 0; j<a.size(); j++){
+        //         System.out.print(a.get(j)+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int maxCnt = 250;
+
+        int totalDuf = 1;
+
+        for (int i = 0; i<3; i++){
+            boolean[] check = new boolean[range+1];
+            int duf = 0;
+            ArrayList<Integer> t = firArr[i];
+            ArrayList<Integer> t2 = secArr[i];
+            for (int j = 0; j<firArr[i].size(); j++){
+                
+                check[t.get(j)] = true;
+            }
+
+            for (int j = 0; j<secArr[i].size(); j++){
+                
+                if(check[t2.get(j)]) {
+                    duf++;
+                }
+            }
+            totalDuf *= duf;
+        }
+
+        System.out.print(maxCnt - totalDuf);
+
+    }
+}
