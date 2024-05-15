735bde52d132c33fc3988f68837ab2f54169783d
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-15 20:52:11 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240515/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+
+        for (int i = 0; i<n; i++){
+            int n1= sc.nextInt();
+            int n2 = sc.nextInt();
+            x[i] = n1;
+            y[i] = n2;
+        }
+
+        int minDis = Integer.MAX_VALUE;
+
+        for (int i = 0; i<n; i++){
+            int dis = 0;
+            for (int j = 0; j<n; j++){
+                if(i==j) continue;
+                int xDif = Math.abs(x[i] - x[j]);
+                int yDif = Math.abs(x[i] - x[j]);
+
+                dis = (int)(Math.pow(xDif,2)) + (int)(Math.pow(yDif,2));
+            }
+            minDis = Math.min(dis, minDis);
+        }
+
+        System.out.print(minDis);
+    }
+}
