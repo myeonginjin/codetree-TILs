2537bd1292a31d466228ed3ae43177d81b63d519
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-15 20:42:40 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240515/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] x = new int[n];
+        int[] y = new int[n];
+
+        for (int i = 0; i<n; i++){
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+
+            x[i] = n1;
+            y[i] = n2;
+        }
+
+        int minSize = Integer.MAX_VALUE;
+        for (int i = 0; i<n; i++){
+            int size = 0;
+
+            int minX = 40001;
+            int maxX = 0;
+            int minY = 40001;
+            int maxY = 0;
+
+            for (int j = 0; j<n; j++){
+                if(i==j) continue;
+
+                maxX = Math.max(maxX, x[j]);
+                minX = Math.min(minX, x[j]);
+
+                maxY = Math.max(maxY, y[j]);
+                minY = Math.min(minY, y[j]);
+            }
+
+            size = (maxY-minY) * (maxX-minX);
+            if(size == 0) continue;
+            minSize = Math.min(minSize, size);
+        }
+        System.out.print(minSize);
+    }
+    
+}
