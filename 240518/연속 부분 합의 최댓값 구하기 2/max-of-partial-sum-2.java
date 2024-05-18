3866ae131837f62ade854eee7eee87e619eaed33
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-18 22:46:02 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240518/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i<n; i++){ 
+            arr[i] = sc.nextInt();
+        }
+
+        int maxSum = 0;
+
+        int sum = 0;
+        for (int i = 0; i<n; i++){
+            sum += arr[i];
+            if(sum < 0) {
+                sum = 0;
+                continue;
+            }
+
+            maxSum = Math.max(sum , maxSum);
+        }
+
+        System.out.print(maxSum);
+    }
+}
