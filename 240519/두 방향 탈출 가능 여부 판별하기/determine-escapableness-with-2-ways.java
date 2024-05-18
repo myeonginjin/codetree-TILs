6467ef08aafb3ee6f763c3e18ff5b6761771b4fc
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-05-19 00:00:57 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240519/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    public static int n,m;
+    public static int[][] arr = new int[101][101];
+    public static int[][] visited = new int[101][101];
+
+
+    public static boolean canGo(int r, int c){
+        return r >=0 && r < n && c >= 0 && c < m;
+    }
+
+
+    public static void move(int r , int c){
+        int[] dr = new int[]{1,0};
+        int[] dc = new int[]{0,1};
+
+
+        for (int i = 0; i<2; i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(canGo(nr,nc) && arr[nr][nc] != 1){
+                visited[nr][nc] = 1;
+                move(nr, nc);
+            }
+        }
+
+
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<m; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        move(0,0);
+        visited[0][0] = 1;
+
+        System.out.print(visited[n-1][m-1]);
+    }
+}
