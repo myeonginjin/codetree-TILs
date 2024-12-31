5f500959254fd97f44ab2286b8f04e579de2ac9f
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-12-31 14:44:53 +0900

    Title: 최대 증가 부분 수열, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241231/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 241231/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -2,36 +2,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int n;
+    static int[] dp;
+    static int[] arr;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str;
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
         str = br.readLine();
         st = new StringTokenizer(str);
 
-        int[] arr = new int[n];
+        arr = new int[n+1];
+        dp = new int[n+1];
 
-        for (int i = 0; i<n; i++){
+        for (int i = 1; i<=n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        init();
 
-        int[] dp = new int[n];
-        dp[0] = 1;
+        for (int i = 1; i<=n; i++) {
+            int elm = arr[i];
 
-        for (int i = 1; i<n; i++){
-            int node = arr[i];
-            dp[i] = 1;
-
-            for (int j = 0; j<=i; j++) {
-                if(node > arr[j]) dp[i] = Math.max( dp[i], dp[j] + 1 );
-                //else dp[i] = Math.max(dp[i], dp[j]);
+            for (int j = 0; j<i; j++) {
+                if(elm > dp[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
             }
         }
-        Arrays.sort(dp);
-        System.out.print(dp[n-1]);
 
+        Arrays.sort(dp);
+        System.out.print(dp[n]);
+    }
+
+    static void init() {
+        dp[0] = 0;
+        for (int i = 1; i<=n; i++) dp[i] = Integer.MIN_VALUE;
     }
 }
