89cc636cb4c63b72378cfc2c612b24a3db147b55
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-05-19 17:52:44 +0900

    Title: 정수 명령 처리 5, Time: 304ms, Memory: 16MB, Status: Passed - Codetree

--- 250519/정수 명령 처리 5/process-numeric-commands-5.java
+++ 250519/정수 명령 처리 5/process-numeric-commands-5.java
@@ -15,7 +15,7 @@ public class Main {
                 arr.add(num);
             }
             else if(command.equals("pop_back")){
-                arr.remove(v.size()-1);
+                arr.remove(arr.size()-1);
             }else if(command.equals("size")){
                 System.out.println(arr.size());
             }else{
