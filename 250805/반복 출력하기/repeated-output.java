06f1a9689590218d14ee3d3b5cdd1c615a20ddf3
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-08-05 21:23:01 +0900

    Title: 반복 출력하기, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 250805/반복 출력하기/repeated-output.java
+++ 250805/반복 출력하기/repeated-output.java
@@ -4,17 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        for(int i = 0; i<n)
         func(n);
     }
 
     static void func(int a){
         
         for(int i = 0; i <a; i++){
-
+            for(int j = 1; j<=5; j++){
+                System.out.print(j);
+            }
+            System.out.println("^&*()_");
         }
-
-        
     }
 }
