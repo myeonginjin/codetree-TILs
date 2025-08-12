967f3d1baa41abe9b3bc6252ce2598c2cd379451
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-08-12 21:04:46 +0900

    Title: 함수를 이용한 윤년 판별, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250812/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        
+        System.out.println(checkYear(y));
+    }
+
+    static boolean checkYear(int n){
+        if(n % 4 == 0){
+            if(n % 100 == 0 && n % 400 != 0){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
