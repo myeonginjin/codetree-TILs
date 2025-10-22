d6904e6ae30f28ff66eca6a131ff0e25595fe826
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-10-22 09:04:26 +0900

    Title: 2진수로 변환하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251022/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // 2의 20승
+        int[] digits = new int[20];
+        int cnt = 0;
+
+        while(true){
+            if(n<2){
+                digits[cnt++] = 1;
+                break;
+            }
+            digits[cnt++] = n % 2;
+            n /= 2;
+        }
+
+
+        for(int i = n-1; i>= 0; i++){
+            System.out.println(digits[i]);
+        }
+
+    }
+}
