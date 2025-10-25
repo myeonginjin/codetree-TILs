9d0f92f23de7646fab03ed8f87fca7196ed5b4f1
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-10-25 18:15:59 +0900

    Title: 여러가지 진수변환, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251025/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int[] arr = new int[n];
+        int cnt = 0;
+        while(true){
+            if(n < 2){
+                arr[cnt] = n;
+                break;
+            }
+
+            arr[cnt++] = n % b;
+            n = n / 4;
+        }
+
+        for(int i = cnt; i >= 0; i--){
+            System.out.print(arr[i]);
+        }
+
+    }
+}
