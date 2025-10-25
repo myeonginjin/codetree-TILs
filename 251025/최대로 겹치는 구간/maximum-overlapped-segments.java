7a7b04c503ac06cf24bf7b6328a7e5572f19efd7
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-10-25 21:22:04 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251025/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+
+    public static final int OFFSET = 100;
+    public static final int MAX_N = 100;
+    public static final int MAX_R = 200;
+
+    int[] x1 = new int[n];
+    int[] x2 = new int[n];
+
+
+    
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] total = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+        }
+
+
+
+    }
+}
