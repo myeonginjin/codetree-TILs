9a41fc41df2f3fc8e3eb0376bd07ed4bfcf2676f
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-10-25 21:46:29 +0900

    Title: 최대로 겹치는 구간, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

--- 251025/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 251025/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+        int n = sc.nextInt();
 
 
         for (int i = 0; i < n; i++) {
