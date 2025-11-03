bd00e7ba4bece1675804c31f811ec4ce8b8182e7
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-11-03 21:50:30 +0900

    Title: 왔다 갔던 구역 2, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251103/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int OFFSET = 1000;
+        int maxN = N + 100;
+        int maxR = 2000;
+        int[] count = new int[maxR + 1];
+        int[] start = new int[maxN];
+        int[] end = new int[maxN];
+        int cur = 0;
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char dir = sc.next().charAt(0);
+
+            // 현재 위치 부터 다음 위치까지가 필요
+            if(dir == 'L'){
+                start[i] = cur - x;
+                end[i] = cur;
+                cur -= x;
+            }
+
+            if(dir == 'R'){
+                start[i] = cur;
+                end[i] = cur + x;
+                cur += x;
+            }
+
+            start[i] += OFFSET;
+            end[i] += OFFSET;
+        }
+
+        for(int i = 0; i< N; i++){
+            for(int j = start[i]; j < end[i]; j++){
+                count[i]++;
+            }
+        }
+        
+        int answer = 0;
+        for(int i = 0; i<=2000; i++){
+            if(count[i] >= 2){
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
