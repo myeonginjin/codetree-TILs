a1b3637767149c30a992de9b388d9f63d4b9a7c9
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-11-04 21:14:52 +0900

    Title: 신기한 타일 뒤집기, Time: 141ms, Memory: 11MB, Status: Runtime Error - Codetree

--- 251104/신기한 타일 뒤집기/strange-flipping-tiles.java
+++ 251104/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -3,14 +3,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int OFFSET = 100000;
-        
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            char d = sc.next().charAt(0);
-        }
+
+        int[] checked = new int[OFFSET * 2 + 1];
+        int cur = OFFSET;
 
         // 신기한 타일 - 왼쪽으로 뒤집으면 흰색, 오른쪽으로 뒤집으면 검은색
         // 흰색, 검은색 타일 수를 각각 출력
+
+        // 1은 흰색, 2는 검은색
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            if(d == 'L'){
+                while(x-- > 0){
+                    checked[cur] = 1;
+                    if(x > 0) cur--;
+                }
+            }else{
+                while(x-- > 0){
+                    checked[cur] = 2;
+                    if(x > 0) cur++;
+                }
+            }
+        }
+
+        int whiteCount = 0;
+        int blackCount = 0;
+
+        for(int i = 0; i <= OFFSET * 2 + 1; i++){
+            if(checked[i] == 1) whiteCount++;
+            else if(checked[i] == 2) blackCount++;
+        }
+
+        System.out.println(whiteCount + " " + blackCount);
     }
 }
