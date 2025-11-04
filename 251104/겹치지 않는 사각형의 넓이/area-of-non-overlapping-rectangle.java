43a9a9c778d2fe2d449fc1af266a5559f8424299
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-11-04 22:25:27 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 247ms, Memory: 28MB, Status: Passed - Codetree

---
+++ 251104/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int OFFSET = 1000;
+        int[][] arr = new int[OFFSET*2][OFFSET*2];
+        int ax1 = sc.nextInt() + OFFSET;
+        int ay1 = sc.nextInt() + OFFSET;
+        int ax2 = sc.nextInt()+ OFFSET;
+        int ay2 = sc.nextInt()+ OFFSET;
+        int bx1 = sc.nextInt()+ OFFSET;
+        int by1 = sc.nextInt()+ OFFSET;
+        int bx2 = sc.nextInt()+ OFFSET;
+        int by2 = sc.nextInt()+ OFFSET;
+        int mx1 = sc.nextInt()+ OFFSET;
+        int my1 = sc.nextInt()+ OFFSET;
+        int mx2 = sc.nextInt()+ OFFSET;
+        int my2 = sc.nextInt()+ OFFSET;
+
+        for(int i = ax1; i<ax2; i++){
+            for(int j = ay1; j<ay2; j++){
+                arr[i][j] = 1;
+            }
+        }
+
+        for(int i = bx1; i<bx2; i++){
+            for(int j = by1; j<by2; j++){
+                arr[i][j] = 1;
+            }
+        }
+
+        for(int i = mx1; i<mx2; i++){
+            for(int j = my1; j<my2; j++){
+                if(arr[i][j] == 1){
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        int answer = 0;
+        for(int i = 0; i< OFFSET * 2; i++){
+            for(int j = 0; j<OFFSET * 2; j++){
+                if(arr[i][j] == 1) answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
