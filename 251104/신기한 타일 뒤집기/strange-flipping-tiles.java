19cc1de62acc1c9622fbf8fc1bd05d6335355a6c
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-11-04 21:04:13 +0900

    Title: 신기한 타일 뒤집기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251104/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int OFFSET = 100000;
+        
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+        }
+
+        // 신기한 타일 - 왼쪽으로 뒤집으면 흰색, 오른쪽으로 뒤집으면 검은색
+        // 흰색, 검은색 타일 수를 각각 출력
+    }
+}
