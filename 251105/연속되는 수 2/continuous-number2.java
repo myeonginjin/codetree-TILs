d3fd4c5a2311c358bf77e08267bb8fd8d8fadffc
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2025-11-05 21:20:27 +0900

    Title: 연속되는 수 2, Time: 138ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 251105/연속되는 수 2/continuous-number2.java
+++ 251105/연속되는 수 2/continuous-number2.java
@@ -17,11 +17,14 @@ public class Main {
 
         int count = 0;
         for(int i = 0; i<n; i++){
-            if(max == arr[i] && arr[i] == arr[i-1]){
-                count++;
+            if(max == arr[i]){
+                while(arr[i] != arr[i-1]){
+                    count++;
+                    i++;
+                }
             }
         }
 
-        System.out.println(count+1);
+        System.out.println(count);
     }
 }
